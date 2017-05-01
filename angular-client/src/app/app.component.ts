ffe6a7340bf477d58c9297255a26d5242f641b0b
Daniel Blankenship <dblankenship@10-200-36-219.employee-macbook.bluehost.com>
2017-04-30 19:42:35 -0600

    remove mongodb docker image remove references to mongodb

--- angular-client/src/app/app.component.ts
+++ angular-client/src/app/app.component.ts
@@ -15,32 +15,29 @@ export class AppComponent implements OnInit {
   // Link to our api, pointing to localhost
   API = 'http://localhost:3000';
 
-  // Declare empty list of people
-  people: any[] = [];
-
   constructor(private http: Http) {}
 
-  // Angular 2 Life Cycle event when component has been initialized
   ngOnInit() {
     this.getAllPeople();
   }
 
   // Add one person to the API
   addPerson(name, age) {
+    /** Keeping reference
     this.http.post(`${this.API}/users`, {name, age})
       .map(res => res.json())
       .subscribe(() => {
         this.getAllPeople();
       }, error => console.log(error))
+    */    
   }
 
   // Get all users from the API
   getAllPeople() {
-    this.http.get(`${this.API}/users`)
+    this.http.get(`${this.API}/`)
       .map(res => res.json())
       .subscribe(people => {
         console.log(people)
-        this.people = people
       }, error => console.log(error))
   }
 }
--- express-server/routes/api.js
+++ express-server/routes/api.js
@@ -1,63 +1,10 @@
 // Import dependencies
-const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-// MongoDB URL from the docker-compose file
-// const dbHost = 'mongodb://database/mean-docker';
-const dbHost = 'mongodb://database:27017';
-// const dbHost = 'mongodb://172.21.0.3:27018';
-//const dbHost = 'mongodb://mongo:27017';
-
-// Connect to mongodb
-mongoose.connect(dbHost);
-
-// create mongoose schema
-const userSchema = new mongoose.Schema({
-  name: String,
-  age: Number
-});
-
-// create mongoose model
-const User = mongoose.model('User', userSchema);
-
 /* GET api listing. */
 router.get('/', (req, res) => {
-		res.send('api works');
-});
-
-/* GET all users. */
-router.get('/users', (req, res) => {
-	User.find({}, (err, users) => {
-		if (err) res.status(500).send(error)
-
-		res.status(200).json(users);
-	});
-});
-
-/* GET one users. */
-router.get('/users/:id', (req, res) => {
-	User.findById(req.params.id, (err, users) => {
-		if (err) res.status(500).send(error)
-
-		res.status(200).json(users);
-	});
-});
-
-/* Create a user. */
-router.post('/users', (req, res) => {
-	let user = new User({
-		name: req.body.name,
-		age: req.body.age
-	});
-
-	user.save(error => {
-		if (error) res.status(500).send(error);
-
-		res.status(201).json({
-			message: 'User created successfully'
-		});
-	});
+  res.json('api works');
 });
 
 module.exports = router;
